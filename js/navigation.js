21772366c93d39e348f76a96bd5afdf405337ccc
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-04-15 13:50:52 +0100

    Commeting

--- js/navigation.js
+++ js/navigation.js
@@ -1,42 +1,58 @@
-//Será executado quando a página estiver carregada
+// Runs when page get full loaded
+// Será executado quando a página estiver carregada
 window.addEventListener("load", () => {
 
-    //Pegar os elementos da barra de navegação
+    // Get all elements from navbar
+    // Pegar os elementos da barra de navegação
     var homeElement = document.getElementById("home");
     var contactElement = document.getElementById("contact");
     var aboutElement = document.getElementById("about");
 
-    //Pegar o nome do ficheiro html da página corrente
+    // Get file name from current page
+    // Pegar o nome do ficheiro html da página corrente
     var path = window.location.pathname;
     var page = path.split("/").pop();
 
-    //console.log(page);
+    // console.log(page);
 
-    /*
-    Se a página que eu quero aceder for a página index.html adiciona a ela as classes com o CSS necessário,
-    caso contrário remove as classes com o CSS necessário.
-     */
-    if (page === "index.html") {
+    // If we are at home page
+    // Se estivermos na página principal
+    if (path === '/') {
         homeElement.classList.add("right");
         homeElement.children[0].classList.add("active");
     } else {
-        homeElement.classList.remove("right");
-        homeElement.children[0].classList.remove("active");
-    }
 
-    if (page === "contact.html") {
-        contactElement.classList.add("right");
-        contactElement.children[0].classList.add("active");
-    } else {
-        contactElement.classList.remove("right");
-        contactElement.children[0].classList.remove("active");
-    }
+        /*
+            If the page I want to access to the index.html page add to it the classes with the necessary CSS,
+            otherwise remove the classes with the necessary CSS.
+         */
 
-    if (page === "about.html") {
-        aboutElement.classList.add("right");
-        aboutElement.children[0].classList.add("active");
-    } else {
-        aboutElement.classList.remove("right");
-        aboutElement.children[0].classList.remove("active");
+        /*
+            Se a página que eu quero aceder for a página index.html adiciona a ela as classes com o CSS necessário,
+            caso contrário remove as classes com o CSS necessário.
+        */
+        if (page === "index.html") {
+            homeElement.classList.add("right");
+            homeElement.children[0].classList.add("active");
+        } else {
+            homeElement.classList.remove("right");
+            homeElement.children[0].classList.remove("active");
+        }
+
+        if (page === "contact.html") {
+            contactElement.classList.add("right");
+            contactElement.children[0].classList.add("active");
+        } else {
+            contactElement.classList.remove("right");
+            contactElement.children[0].classList.remove("active");
+        }
+
+        if (page === "about.html") {
+            aboutElement.classList.add("right");
+            aboutElement.children[0].classList.add("active");
+        } else {
+            aboutElement.classList.remove("right");
+            aboutElement.children[0].classList.remove("active");
+        }
     }
 });
