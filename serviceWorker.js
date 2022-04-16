fd29c51a4769b619d6c5f12e646b3dfb71935a69
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-04-16 15:23:41 +0100

    Added PWA

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,20 +1,20 @@
 const ticTacToe = "Tic Tac Toe"
 const assets = [
-  "./",
-  "./index.html",
-  "./pages/about.html",
-  "./pages/contact.html",
-  "./css/style.css",
-  "./css/prism.css",
-  "./js/app.js",
-  "./js/jquery-3.6.0.min.js",
-  "./js/prism.js",
-  "./js/navigation.js",
-  "./js/ttt.js",
-  "./imgs/diagram.png",
-  "./imgs/avatar.png",
-  "./sound/pencil_o.mp3",
-  "./sound/pencil_x.mp3"
+  "/TicTacToe/",
+  "/TicTacToe/index.html",
+  "/TicTacToe/pages/about.html",
+  "/TicTacToe/pages/contact.html",
+  "/TicTacToe/css/style.css",
+  "/TicTacToe/css/prism.css",
+  "/TicTacToe/js/app.js",
+  "/TicTacToe/js/jquery-3.6.0.min.js",
+  "/TicTacToe/js/prism.js",
+  "/TicTacToe/js/navigation.js",
+  "/TicTacToe/js/ttt.js",
+  "/TicTacToe/imgs/diagram.png",
+  "/TicTacToe/imgs/avatar.png",
+  "/TicTacToe/sound/pencil_o.mp3",
+  "/TicTacToe/sound/pencil_x.mp3"
 ]
 
 self.addEventListener("install", installEvent => {
