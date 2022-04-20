e54a0ec89250a233e4647c2642c2943299628bf8
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-04-20 17:14:35 +0100

    Added PWA support

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/TicTacToe/serviceWorker.js", {scope: '/TicTacToe/'})
-        .then(res => console.log("service worker registered"))
-        .catch(err => console.log("service worker not registered", err))
-    })
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register ('/TicTacToe/serviceWorker.js', {scope: '/TicTacToe/'})
+    .then(res => console.log("service worker registered"))
+    .catch(err => console.log("service worker not registered", err))
   }
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-var GHPATH = '${./TicTacToe}';
+var GHPATH = '${/TicTacToe}';
  
 var APP_PREFIX = 'TicTacToe_';
 
@@ -18,40 +18,41 @@ var URLS = [
   `${GHPATH}/js/prism.js`,
   `${GHPATH}/js/navigation.js`,
   `${GHPATH}/js/ttt.js`,
+  `${GHPATH}/serviceWorker.js`,
   `${GHPATH}/imgs/diagram.png`,
   `${GHPATH}/imgs/avatar.png`,
   `${GHPATH}/sound/pencil_o.mp3`,
   `${GHPATH}/sound/pencil_x.mp3`
-]
+];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+  console.log('fetch request : ' + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
+        console.log('responding with cache : ' + e.request.url);
+        return request;
       } else {       // if there are no cache, try fetching request
         console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
+        return fetch(e.request);
       }
 
       // You can omit if/else for console.log & put one line below like this too.
       // return request || fetch(e.request)
     })
-  )
-})
+  );
+});
 
 // Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
+      console.log('installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS);
     })
-  )
-})
+  );
+});
 
 // Delete outdated caches
 self.addEventListener('activate', function (e) {
@@ -61,16 +62,17 @@ self.addEventListener('activate', function (e) {
       // filter out ones that has this app prefix to create white list
       var cacheWhitelist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX)
-      })
+      });
       // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
+      cacheWhitelist.push(CACHE_NAME);
 
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
         }
-      }))
+      }));
     })
-  )
-})
+  );
+  e.waitUntil(clients.claim());
+});
