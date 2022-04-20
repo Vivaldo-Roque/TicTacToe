e188f0f199356f735581d23e9fc71c1285a42c01
Vivaldo-Roque <2001vivaldo@gmail.com>
2022-04-20 17:20:26 +0100

    Added PWA support

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-if (navigator.serviceWorker) {
+window.addEventListener("load", () => {
+  if (navigator.serviceWorker) {
     navigator.serviceWorker.register ('/TicTacToe/serviceWorker.js', {scope: '/TicTacToe/'})
     .then(res => console.log("service worker registered"))
     .catch(err => console.log("service worker not registered", err))
   }
+});
